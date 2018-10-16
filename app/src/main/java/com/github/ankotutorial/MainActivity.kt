3efcf2943fbf41b4214895bbe684b3304f5d45fa
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-10-16 22:03:07 +0700

    Add temp image, Mod MainActivity: Add MainActivityUI

--- app/src/main/java/com/github/ankotutorial/MainActivity.kt
+++ app/src/main/java/com/github/ankotutorial/MainActivity.kt
@@ -2,11 +2,31 @@ package com.github.ankotutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MainActivityUI().setContentView(this)
     }
+
+    class MainActivityUI: AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+            verticalLayout {
+                imageView(R.drawable.ic_android_black_24dp).lparams(width = matchParent) {
+                    padding = dip(20)
+                    margin = dip(15)
+                }
+
+                val name = editText() {
+                    hint = "what's your name?"
+                }
+
+                button("Say Hello")
+            }
+        }
+    }
+
 }
